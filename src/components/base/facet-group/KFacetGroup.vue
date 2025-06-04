3a5ba7417ae883ba348825210fa3acbd7ed5c723
Bachmann <lisa.bachmann@fokus.fraunhofer.de>
2025-06-04 11:24:07 +0200

    refactor KFacetGroup

--- src/components/base/facet-group/KFacetGroup.vue
+++ src/components/base/facet-group/KFacetGroup.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import type { PanelProps } from 'primevue/panel'
 import type { Facet } from '../../../utils/types'
 import { useDataTruncator } from '@/composables/useDataTruncator'
 import PhCaretDown from '~icons/ph/caret-down'
@@ -28,62 +27,6 @@ const {
   limit: 5,
 })
 
-const panelPreset = {
-  header: ({ props }: { props: PanelProps }) => ({
-    class: [
-      // Flex
-      'flex items-center justify-between',
-      // Colors
-      'text-surface-700 dark:text-surface-0/80',
-      'bg-transparent',
-      // 'border border-surface-200 dark:border-surface-700',
-      // Shape
-      'rounded-tl-lg rounded-tr-lg',
-      // Conditional Spacing
-      { 'px-4 py-3': props.toggleable },
-    ],
-  }),
-  title: {
-    class: 'leading-none font-bold',
-  },
-  toggler: {
-    class: [
-      'inline-flex items-center justify-center',
-      'relative',
-      'w-8 h-8',
-      'm-0 p-0',
-      'border-0 rounded-full',
-      'bg-transparent',
-      'text-surface-600 dark:text-surface-0/80',
-      'focus:outline-hidden focus:outline-offset-0 focus-visible:ring-3 focus-visible:ring-primary-400/50 focus-visible:ring-inset dark:focus-visible:ring-primary-300/50',
-      'transition-all duration-200 ease-in-out',
-      'overflow-hidden no-underline',
-      'cursor-pointer',
-    ],
-  },
-  togglerIcon: {
-    class: 'inline-block',
-  },
-  content: {
-    class: ['text-surface-700 dark:text-surface-0/80'],
-  },
-  footer: {
-    class: [
-      'py-3 p-5',
-      'border border-t-0 rounded-br-lg rounded-bl-lg',
-      'border-surface-200 dark:border-surface-700',
-      'text-surface-700 dark:text-surface-0/80',
-    ],
-  },
-  transition: {
-    enterFromClass: 'max-h-0',
-    enterActiveClass: 'overflow-hidden transition-[max-height] duration-1000 ease-[cubic-bezier(0.42,0,0.58,1)]',
-    enterToClass: 'max-h-[1000px]',
-    leaveFromClass: 'max-h-[1000px]',
-    leaveActiveClass: 'overflow-hidden transition-[max-height] duration-[450ms] ease-[cubic-bezier(0,1,0,1)]',
-    leaveToClass: 'max-h-0',
-  },
-}
 </script>
 
 <template>
