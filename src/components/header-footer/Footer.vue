91220d63e11308808e10d608912f697b9d23f076
Nosova <evelin.nosova@fokus.fraunhofer.de>
2026-04-23 10:48:22 +0200

    fix: footer

--- src/components/header-footer/Footer.vue
+++ src/components/header-footer/Footer.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
@@ -30,24 +31,83 @@ const loginLinks = computed(() => [
 </script>
 
 <template>
-  <footer class="bg-footer-bg px-8 py-16 text-footer-bg-text md:px-30">
-    <div class="container mx-auto max-w-7xl">
-  
-      <div class="flex flex-col flex-wrap justify-between gap-x-10 gap-y-20 md:flex-row">
-        
-        <!-- Login Section (links) -->
-        <div class="w-full md:w-3/12 md:mr-10 xl:w-2/12">
-          <img src="/piveau-logo.png" alt="piveau logo" class="mb-6 w-24">
+  <footer class="bg-footer-bg px-8 py-16 text-footer-bg-text">
+    <div class="container mx-auto max-w-7xl space-y-32">
+      <div
+        class="
+          flex flex-col flex-wrap justify-between gap-x-10 gap-y-20
+          md:flex-row
+          xl:gap-10
+        "
+      >
+        <!-- Login Section -->
+        <div
+          class="
+            w-full
+            md:w-3/12
+            xl:w-2/12
+          "
+        >
+        <img src="/piveau-logo.png" alt="piveau logo" class="mb-6 w-24">
           <ul class="flex flex-col gap-4">
-            <li v-for="link in loginLinks" :key="link.text" class="text-copy-sm">
+            <li
+              v-for="link in loginLinks"
+              :key="link.text"
+              class="text-copy-sm"
+            >
               <template v-if="link.href">
-                <a :href="link.href" target="_blank" rel="noopener noreferrer"
-                   class="font-bold hover:text-secondary-hover">
+        <a
+          :href="link.href"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="font-bold hover:text-secondary-hover"
+        >
+          {{ link.text }}
+        </a>
+      </template>
+      <template v-else>
+        <RouterLink
+          :to="link.to"
+          class="font-bold hover:text-secondary-hover"
+        >
+          {{ link.text }}
+        </RouterLink>
+      </template>
+            </li>
+          </ul>
+        </div>
+        <!-- Sitemap Section -->
+        <div
+          class="
+            mr-10 w-full
+            md:w-3/12
+            xl:w-2/12
+          "
+        >
+          <h3 class="mb-4 text-xl font-semibold">
+            {{ t('footer.sections.sitemap') }}
+          </h3>
+          <ul class="flex flex-col gap-0">
+            <li
+              v-for="link in seitenLinks"
+              :key="link.text"
+              class="text-copy-sm"
+            >
+              <template v-if="link.to.startsWith('http')">
+                <a
+                  :href="link.to"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="hover:text-secondary-hover"
+                >
                   {{ link.text }}
                 </a>
               </template>
               <template v-else>
-                <RouterLink :to="link.to" class="font-bold hover:text-secondary-hover">
+                <RouterLink
+                  :to="link.to"
+                  class="hover:text-secondary-hover"
+                >
                   {{ link.text }}
                 </RouterLink>
               </template>
@@ -55,59 +115,62 @@ const loginLinks = computed(() => [
           </ul>
         </div>
 
-        
-        <div class="flex flex-col flex-wrap gap-x-32 gap-y-20 md:flex-row">
-          <!-- Sitemap -->
-          <div>
-            <h3 class="mb-4 text-xl font-semibold">
-              {{ t('footer.sections.sitemap') }}
-            </h3>
-            <ul class="flex flex-col">
-              <li v-for="link in seitenLinks" :key="link.text" class="text-copy-sm">
-                <template v-if="link.to.startsWith('http')">
-                  <a :href="link.to" target="_blank" rel="noopener noreferrer"
-                     class="hover:text-secondary-hover">
-                    {{ link.text }}
-                  </a>
-                </template>
-                <template v-else>
-                  <RouterLink :to="link.to" class="hover:text-secondary-hover">
-                    {{ link.text }}
-                  </RouterLink>
-                </template>
-              </li>
-            </ul>
-          </div>
-
-          <!-- Social Media -->
-          <div>
-            <h3 class="mb-4 text-xl font-semibold">
-              {{ t('footer.sections.socialMedia') }}
-            </h3>
-            <ul class="flex flex-col gap-0">
-              <li v-for="link in socialLinks" :key="link.text" class="text-copy-sm">
-                <a :href="link.href" target="_blank" class="hover:text-secondary-hover">
-                  {{ link.text }}
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <!-- Legal -->
-          <div>
-            <h3 class="mb-4 text-xl font-semibold">
-              {{ t('footer.sections.legal') }}
-            </h3>
-            <ul class="flex flex-col gap-0">
-              <li v-for="link in rechtlichesLinks" :key="link.text" class="text-copy-sm">
-                <RouterLink :to="link.to" class="hover:text-secondary-hover">
-                  {{ link.text }}
-                </RouterLink>
-              </li>
-            </ul>
-          </div>
-
+        <!-- Social Media Section -->
+        <div
+          class="
+            w-full
+            md:w-3/12
+            xl:w-2/12
+          "
+        >
+          <h3 class="mb-4 text-xl font-semibold">
+            {{ t('footer.sections.socialMedia') }}
+          </h3>
+          <ul class="flex flex-col gap-0">
+            <li
+              v-for="link in socialLinks"
+              :key="link.text"
+              class="text-copy-sm"
+            >
+              <a
+                :href="link.href"
+                target="_blank"
+                class="hover:text-secondary-hover"
+              >
+                {{ link.text }}
+              </a>
+            </li>
+          </ul>
         </div>
+
+        <!-- Legal Section -->
+        <div
+          class="
+            mr-10 w-full
+            md:w-3/12
+            xl:w-2/12
+          "
+        >
+          <h3 class="mb-4 text-xl font-semibold">
+            {{ t('footer.sections.legal') }}
+          </h3>
+          <ul class="flex flex-col gap-0">
+            <li
+              v-for="link in rechtlichesLinks"
+              :key="link.text"
+              class="text-copy-sm"
+            >
+              <RouterLink
+                :to="link.to"
+                class="hover:text-secondary-hover"
+              >
+                {{ link.text }}
+              </RouterLink>
+            </li>
+          </ul>
+        </div>
+
+      
       </div>
     </div>
   </footer>
